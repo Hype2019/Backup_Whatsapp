53bf53eef278efb30478297dd0883325a6b5ebd8
Hype2019 <hypetelecom@outlook.com>
2025-04-04 11:06:47 -0300

    Starting project

---
+++ src/test/kotlin/br/com/hypetelecom/Backup_Whatsapp/BackupWhatsappApplicationTests.kt
@@ -0,0 +1,13 @@
+package br.com.hypetelecom.Backup_Whatsapp
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class BackupWhatsappApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
