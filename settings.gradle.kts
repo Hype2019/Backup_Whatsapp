b73e88293a8c496d00c94c86b596448a029e9711
Hype2019 <hypetelecom@outlook.com>
2025-04-07 09:26:51 -0300

    Starting project

---
+++ settings.gradle.kts
@@ -0,0 +1,1 @@
+rootProject.name = "Backup_Whatsapp"
---
+++ src/main/kotlin/br/com/hypetelecom/Backup_Whatsapp/BackupWhatsappApplication.kt
@@ -0,0 +1,11 @@
+package br.com.hypetelecom.Backup_Whatsapp
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class BackupWhatsappApplication
+
+fun main(args: Array<String>) {
+	runApplication<BackupWhatsappApplication>(*args)
+}
